1fe6b160670a534d94d542bc789590fc10cf291d
darkweird <foxflameless@gmail.com>
2021-09-09 11:18:55 +0300

    feature(gestaltv7-events): replace ReceiveEvent with gestaltv7's ReceiveEvent

--- src/main/java/org/terasology/computer/display/system/client/DisplayClientSystem.java
+++ src/main/java/org/terasology/computer/display/system/client/DisplayClientSystem.java
@@ -12,7 +12,6 @@ import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.entitySystem.entity.lifecycleEvents.BeforeRemoveComponent;
 import org.terasology.engine.entitySystem.entity.lifecycleEvents.OnAddedComponent;
 import org.terasology.engine.entitySystem.entity.lifecycleEvents.OnChangedComponent;
-import org.terasology.engine.entitySystem.event.ReceiveEvent;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
@@ -27,6 +26,7 @@ import org.terasology.engine.utilities.Assets;
 import org.terasology.engine.world.block.BlockPart;
 import org.terasology.engine.world.block.shapes.BlockMeshPart;
 import org.terasology.engine.world.block.shapes.BlockShape;
+import org.terasology.gestalt.entitysystem.event.ReceiveEvent;
 import org.terasology.nui.Color;
 
 import java.util.HashMap;
